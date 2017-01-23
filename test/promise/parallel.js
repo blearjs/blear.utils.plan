55d1c574e61b96c8fc32b2d30534f907b23c4f1a
#云淡然 <cloudcome@163.com>
2017-01-23 18:10:37 +0800

    + 支持 promise

--- test/promise/parallel.js
+++ test/promise/parallel.js
@@ -21,226 +21,230 @@ Promise.prototype.then = function (resolved, rejected) {
     });
 };
 
-it('.taskPromise x 1 resolve', function (done) {
-    plan
-        .taskPromise(function () {
-            return new Promise(function (resolve) {
-                resolve(1);
+describe('parallel', function () {
+    it('.taskPromise x 1 resolve', function (done) {
+        plan
+            .taskPromise(function () {
+                return new Promise(function (resolve) {
+                    resolve(1);
+                });
+            })
+            .parallel()
+            .try(function (result) {
+                expect(result).toBe(1);
+                done();
             });
-        })
-        .serial()
-        .try(function (result) {
-            expect(result).toBe(1);
-            done();
-        });
-});
+    });
 
-it('.taskPromise x 1 reject', function (done) {
-    plan
-        .taskPromise(function () {
-            return new Promise(function (resolve, reject) {
-                reject(new Error('1'));
+    it('.taskPromise x 1 reject', function (done) {
+        plan
+            .taskPromise(function () {
+                return new Promise(function (resolve, reject) {
+                    reject(new Error('1'));
+                });
+            })
+            .parallel()
+            .catch(function (err) {
+                expect(err.message).toBe('1');
+                done();
             });
-        })
-        .serial()
-        .catch(function (err) {
-            expect(err.message).toBe('1');
-            done();
-        });
-});
+    });
 
-it('.taskPromise x 3 resolve', function (done) {
-    plan
-        .taskPromise(function () {
-            return new Promise(function (resolve) {
-                // 1
-                resolve(1);
+    it('.taskPromise x 3 resolve', function (done) {
+        plan
+            .taskPromise(function () {
+                return new Promise(function (resolve) {
+                    // 1
+                    resolve(1);
+                });
+            })
+            .taskPromise(function () {
+                return new Promise(function (resolve) {
+                    // 2
+                    resolve(2);
+                });
+            })
+            .taskPromise(function () {
+                return new Promise(function (resolve) {
+                    // 3
+                    resolve(3);
+                });
+            })
+            .parallel()
+            .try(function (ret1, ret2, ret3) {
+                expect(ret1).toBe(1);
+                expect(ret2).toBe(2);
+                expect(ret3).toBe(3);
+                done();
             });
-        })
-        .taskPromise(function (prev) {
-            return new Promise(function (resolve) {
+    });
+
+    it('.taskPromise x 3 reject', function (done) {
+        plan
+            .taskPromise(function () {
+                return new Promise(function (resolve, reject) {
+                    resolve(1);
+                });
+            })
+            .taskPromise(function () {
+                return new Promise(function (resolve, reject) {
+                    resolve(2);
+                });
+            })
+            .taskPromise(function () {
+                return new Promise(function (resolve, reject) {
+                    reject(new Error('3'));
+                });
+            })
+            .parallel()
+            .catch(function (err) {
+                expect(err.message).toBe('3');
+                done();
+            });
+    });
+
+    it('.taskPromise + .task', function (done) {
+        plan
+            .taskPromise(function () {
+                return new Promise(function (resolve) {
+                    // 1
+                    resolve(1);
+                });
+            })
+            .task(function (next, prev) {
                 // 2 + 1
-                resolve(2 + prev);
+                next(null, 2 + prev);
+            })
+            .serial()
+            .try(function (result) {
+                expect(result).toBe(3);
+                done();
             });
-        })
-        .taskPromise(function (prev) {
-            return new Promise(function (resolve) {
-                // 3 + 3
-                resolve(3 + prev);
-            });
-        })
-        .serial()
-        .try(function (result) {
-            expect(result).toBe(6);
-            done();
-        });
+    });
+
+// it('.taskPromise + .task + .taskSync', function (done) {
+//     plan
+//         .taskPromise(function () {
+//             return new Promise(function (resolve) {
+//                 // 1
+//                 resolve(1);
+//             });
+//         })
+//         .task(function (next, prev) {
+//             // 2 + 1
+//             next(null, 2 + prev);
+//         })
+//         .taskSync(function (prev) {
+//             // 3 + 3
+//             return 3 + prev;
+//         })
+//         .serial()
+//         .try(function (result) {
+//             expect(result).toBe(6);
+//             done();
+//         });
+// });
+//
+// it('.taskPromise + .task + .taskSync + .each', function (done) {
+//     plan
+//         .taskPromise(function () {
+//             return new Promise(function (resolve) {
+//                 // 1
+//                 resolve(1);
+//             });
+//         })
+//         .task(function (next, prev) {
+//             // 2 + 1
+//             next(null, 2 + prev);
+//         })
+//         .taskSync(function (prev) {
+//             // 3 + 3
+//             return 3 + prev;
+//         })
+//         .each([4, 5], function (index, val, next, prev) {
+//             // 第 1 次：4 + 6
+//             // 第 2 次：5 + 10
+//             next(null, val + prev);
+//         })
+//         .serial()
+//         .try(function (result) {
+//             expect(result).toBe(15);
+//             done();
+//         });
+// });
+//
+// it('.taskPromise + .task + .taskSync + .each + .eachSync', function (done) {
+//     plan
+//         .taskPromise(function () {
+//             return new Promise(function (resolve) {
+//                 // 1
+//                 resolve(1);
+//             });
+//         })
+//         .task(function (next, prev) {
+//             // 2 + 1
+//             next(null, 2 + prev);
+//         })
+//         .taskSync(function (prev) {
+//             // 3 + 3
+//             return 3 + prev;
+//         })
+//         .each([4, 5], function (index, val, next, prev) {
+//             // 第 1 次：4 + 6
+//             // 第 2 次：5 + 10
+//             next(null, val + prev);
+//         })
+//         .eachSync([6, 7], function (index, val, prev) {
+//             // 第 1 次：6 + 15
+//             // 第 2 次：7 + 21
+//             return val + prev;
+//         })
+//         .serial()
+//         .try(function (result) {
+//             expect(result).toBe(28);
+//             done();
+//         });
+// });
+//
+// it('.taskPromise + .task + .taskSync + .each + .eachSync + .eachPromise', function (done) {
+//     plan
+//         .taskPromise(function () {
+//             return new Promise(function (resolve) {
+//                 // 1
+//                 resolve(1);
+//             });
+//         })
+//         .task(function (next, prev) {
+//             // 2 + 1
+//             next(null, 2 + prev);
+//         })
+//         .taskSync(function (prev) {
+//             // 3 + 3
+//             return 3 + prev;
+//         })
+//         .each([4, 5], function (index, val, next, prev) {
+//             // 第 1 次：4 + 6
+//             // 第 2 次：5 + 10
+//             next(null, val + prev);
+//         })
+//         .eachSync([6, 7], function (index, val, prev) {
+//             // 第 1 次：6 + 15
+//             // 第 2 次：7 + 21
+//             return val + prev;
+//         })
+//         .eachPromise([8, 9], function (index, val, prev) {
+//             return new Promise(function (resolve) {
+//                 // 第 1 次：8 + 28
+//                 // 第 2 次：9 + 36
+//                 resolve(val + prev);
+//             });
+//         })
+//         .serial()
+//         .try(function (result) {
+//             expect(result).toBe(45);
+//             done();
+//         });
+// });
+
+
 });
-
-it('.taskPromise x 3 reject', function (done) {
-    plan
-        .taskPromise(function () {
-            return new Promise(function (resolve, reject) {
-                resolve(1);
-            });
-        })
-        .taskPromise(function () {
-            return new Promise(function (resolve, reject) {
-                resolve(2);
-            });
-        })
-        .taskPromise(function () {
-            return new Promise(function (resolve, reject) {
-                reject(new Error('3'));
-            });
-        })
-        .serial()
-        .catch(function (err) {
-            expect(err.message).toBe('3');
-            done();
-        });
-});
-
-it('.taskPromise + .task', function (done) {
-    plan
-        .taskPromise(function () {
-            return new Promise(function (resolve) {
-                // 1
-                resolve(1);
-            });
-        })
-        .task(function (next, prev) {
-            // 2 + 1
-            next(null, 2 + prev);
-        })
-        .serial()
-        .try(function (result) {
-            expect(result).toBe(3);
-            done();
-        });
-});
-
-it('.taskPromise + .task + .taskSync', function (done) {
-    plan
-        .taskPromise(function () {
-            return new Promise(function (resolve) {
-                // 1
-                resolve(1);
-            });
-        })
-        .task(function (next, prev) {
-            // 2 + 1
-            next(null, 2 + prev);
-        })
-        .taskSync(function (prev) {
-            // 3 + 3
-            return 3 + prev;
-        })
-        .serial()
-        .try(function (result) {
-            expect(result).toBe(6);
-            done();
-        });
-});
-
-it('.taskPromise + .task + .taskSync + .each', function (done) {
-    plan
-        .taskPromise(function () {
-            return new Promise(function (resolve) {
-                // 1
-                resolve(1);
-            });
-        })
-        .task(function (next, prev) {
-            // 2 + 1
-            next(null, 2 + prev);
-        })
-        .taskSync(function (prev) {
-            // 3 + 3
-            return 3 + prev;
-        })
-        .each([4, 5], function (index, val, next, prev) {
-            // 第 1 次：4 + 6
-            // 第 2 次：5 + 10
-            next(null, val + prev);
-        })
-        .serial()
-        .try(function (result) {
-            expect(result).toBe(15);
-            done();
-        });
-});
-
-it('.taskPromise + .task + .taskSync + .each + .eachSync', function (done) {
-    plan
-        .taskPromise(function () {
-            return new Promise(function (resolve) {
-                // 1
-                resolve(1);
-            });
-        })
-        .task(function (next, prev) {
-            // 2 + 1
-            next(null, 2 + prev);
-        })
-        .taskSync(function (prev) {
-            // 3 + 3
-            return 3 + prev;
-        })
-        .each([4, 5], function (index, val, next, prev) {
-            // 第 1 次：4 + 6
-            // 第 2 次：5 + 10
-            next(null, val + prev);
-        })
-        .eachSync([6, 7], function (index, val, prev) {
-            // 第 1 次：6 + 15
-            // 第 2 次：7 + 21
-            return val + prev;
-        })
-        .serial()
-        .try(function (result) {
-            expect(result).toBe(28);
-            done();
-        });
-});
-
-it('.taskPromise + .task + .taskSync + .each + .eachSync + .eachPromise', function (done) {
-    plan
-        .taskPromise(function () {
-            return new Promise(function (resolve) {
-                // 1
-                resolve(1);
-            });
-        })
-        .task(function (next, prev) {
-            // 2 + 1
-            next(null, 2 + prev);
-        })
-        .taskSync(function (prev) {
-            // 3 + 3
-            return 3 + prev;
-        })
-        .each([4, 5], function (index, val, next, prev) {
-            // 第 1 次：4 + 6
-            // 第 2 次：5 + 10
-            next(null, val + prev);
-        })
-        .eachSync([6, 7], function (index, val, prev) {
-            // 第 1 次：6 + 15
-            // 第 2 次：7 + 21
-            return val + prev;
-        })
-        .eachPromise([8, 9], function (index, val, prev) {
-            return new Promise(function (resolve) {
-                // 第 1 次：8 + 28
-                // 第 2 次：9 + 36
-                resolve(val + prev);
-            });
-        })
-        .serial()
-        .try(function (result) {
-            expect(result).toBe(45);
-            done();
-        });
-});
-
-
--- test/promise/serial.js
+++ test/promise/serial.js
@@ -21,226 +21,228 @@ Promise.prototype.then = function (resolved, rejected) {
     });
 };
 
-it('.taskPromise x 1 resolve', function (done) {
-    plan
-        .taskPromise(function () {
-            return new Promise(function (resolve) {
-                resolve(1);
-            });
-        })
-        .serial()
-        .try(function (result) {
-            expect(result).toBe(1);
-            done();
-        });
-});
+describe('serial', function () {
 
-it('.taskPromise x 1 reject', function (done) {
-    plan
-        .taskPromise(function () {
-            return new Promise(function (resolve, reject) {
-                reject(new Error('1'));
+    it('.taskPromise x 1 resolve', function (done) {
+        plan
+            .taskPromise(function () {
+                return new Promise(function (resolve) {
+                    resolve(1);
+                });
+            })
+            .serial()
+            .try(function (result) {
+                expect(result).toBe(1);
+                done();
             });
-        })
-        .serial()
-        .catch(function (err) {
-            expect(err.message).toBe('1');
-            done();
-        });
-});
+    });
 
-it('.taskPromise x 3 resolve', function (done) {
-    plan
-        .taskPromise(function () {
-            return new Promise(function (resolve) {
-                // 1
-                resolve(1);
+    it('.taskPromise x 1 reject', function (done) {
+        plan
+            .taskPromise(function () {
+                return new Promise(function (resolve, reject) {
+                    reject(new Error('1'));
+                });
+            })
+            .serial()
+            .catch(function (err) {
+                expect(err.message).toBe('1');
+                done();
             });
-        })
-        .taskPromise(function (prev) {
-            return new Promise(function (resolve) {
+    });
+
+    it('.taskPromise x 3 resolve', function (done) {
+        plan
+            .taskPromise(function () {
+                return new Promise(function (resolve) {
+                    // 1
+                    resolve(1);
+                });
+            })
+            .taskPromise(function (prev) {
+                return new Promise(function (resolve) {
+                    // 2 + 1
+                    resolve(2 + prev);
+                });
+            })
+            .taskPromise(function (prev) {
+                return new Promise(function (resolve) {
+                    // 3 + 3
+                    resolve(3 + prev);
+                });
+            })
+            .serial()
+            .try(function (result) {
+                expect(result).toBe(6);
+                done();
+            });
+    });
+
+    it('.taskPromise x 3 reject', function (done) {
+        plan
+            .taskPromise(function () {
+                return new Promise(function (resolve, reject) {
+                    resolve(1);
+                });
+            })
+            .taskPromise(function () {
+                return new Promise(function (resolve, reject) {
+                    resolve(2);
+                });
+            })
+            .taskPromise(function () {
+                return new Promise(function (resolve, reject) {
+                    reject(new Error('3'));
+                });
+            })
+            .serial()
+            .catch(function (err) {
+                expect(err.message).toBe('3');
+                done();
+            });
+    });
+
+    it('.taskPromise + .task', function (done) {
+        plan
+            .taskPromise(function () {
+                return new Promise(function (resolve) {
+                    // 1
+                    resolve(1);
+                });
+            })
+            .task(function (next, prev) {
                 // 2 + 1
-                resolve(2 + prev);
+                next(null, 2 + prev);
+            })
+            .serial()
+            .try(function (result) {
+                expect(result).toBe(3);
+                done();
             });
-        })
-        .taskPromise(function (prev) {
-            return new Promise(function (resolve) {
+    });
+
+    it('.taskPromise + .task + .taskSync', function (done) {
+        plan
+            .taskPromise(function () {
+                return new Promise(function (resolve) {
+                    // 1
+                    resolve(1);
+                });
+            })
+            .task(function (next, prev) {
+                // 2 + 1
+                next(null, 2 + prev);
+            })
+            .taskSync(function (prev) {
                 // 3 + 3
-                resolve(3 + prev);
+                return 3 + prev;
+            })
+            .serial()
+            .try(function (result) {
+                expect(result).toBe(6);
+                done();
             });
-        })
-        .serial()
-        .try(function (result) {
-            expect(result).toBe(6);
-            done();
-        });
+    });
+
+    it('.taskPromise + .task + .taskSync + .each', function (done) {
+        plan
+            .taskPromise(function () {
+                return new Promise(function (resolve) {
+                    // 1
+                    resolve(1);
+                });
+            })
+            .task(function (next, prev) {
+                // 2 + 1
+                next(null, 2 + prev);
+            })
+            .taskSync(function (prev) {
+                // 3 + 3
+                return 3 + prev;
+            })
+            .each([4, 5], function (index, val, next, prev) {
+                // 第 1 次：4 + 6
+                // 第 2 次：5 + 10
+                next(null, val + prev);
+            })
+            .serial()
+            .try(function (result) {
+                expect(result).toBe(15);
+                done();
+            });
+    });
+
+    it('.taskPromise + .task + .taskSync + .each + .eachSync', function (done) {
+        plan
+            .taskPromise(function () {
+                return new Promise(function (resolve) {
+                    // 1
+                    resolve(1);
+                });
+            })
+            .task(function (next, prev) {
+                // 2 + 1
+                next(null, 2 + prev);
+            })
+            .taskSync(function (prev) {
+                // 3 + 3
+                return 3 + prev;
+            })
+            .each([4, 5], function (index, val, next, prev) {
+                // 第 1 次：4 + 6
+                // 第 2 次：5 + 10
+                next(null, val + prev);
+            })
+            .eachSync([6, 7], function (index, val, prev) {
+                // 第 1 次：6 + 15
+                // 第 2 次：7 + 21
+                return val + prev;
+            })
+            .serial()
+            .try(function (result) {
+                expect(result).toBe(28);
+                done();
+            });
+    });
+
+    it('.taskPromise + .task + .taskSync + .each + .eachSync + .eachPromise', function (done) {
+        plan
+            .taskPromise(function () {
+                return new Promise(function (resolve) {
+                    // 1
+                    resolve(1);
+                });
+            })
+            .task(function (next, prev) {
+                // 2 + 1
+                next(null, 2 + prev);
+            })
+            .taskSync(function (prev) {
+                // 3 + 3
+                return 3 + prev;
+            })
+            .each([4, 5], function (index, val, next, prev) {
+                // 第 1 次：4 + 6
+                // 第 2 次：5 + 10
+                next(null, val + prev);
+            })
+            .eachSync([6, 7], function (index, val, prev) {
+                // 第 1 次：6 + 15
+                // 第 2 次：7 + 21
+                return val + prev;
+            })
+            .eachPromise([8, 9], function (index, val, prev) {
+                return new Promise(function (resolve) {
+                    // 第 1 次：8 + 28
+                    // 第 2 次：9 + 36
+                    resolve(val + prev);
+                });
+            })
+            .serial()
+            .try(function (result) {
+                expect(result).toBe(45);
+                done();
+            });
+    });
+
 });
-
-it('.taskPromise x 3 reject', function (done) {
-    plan
-        .taskPromise(function () {
-            return new Promise(function (resolve, reject) {
-                resolve(1);
-            });
-        })
-        .taskPromise(function () {
-            return new Promise(function (resolve, reject) {
-                resolve(2);
-            });
-        })
-        .taskPromise(function () {
-            return new Promise(function (resolve, reject) {
-                reject(new Error('3'));
-            });
-        })
-        .serial()
-        .catch(function (err) {
-            expect(err.message).toBe('3');
-            done();
-        });
-});
-
-it('.taskPromise + .task', function (done) {
-    plan
-        .taskPromise(function () {
-            return new Promise(function (resolve) {
-                // 1
-                resolve(1);
-            });
-        })
-        .task(function (next, prev) {
-            // 2 + 1
-            next(null, 2 + prev);
-        })
-        .serial()
-        .try(function (result) {
-            expect(result).toBe(3);
-            done();
-        });
-});
-
-it('.taskPromise + .task + .taskSync', function (done) {
-    plan
-        .taskPromise(function () {
-            return new Promise(function (resolve) {
-                // 1
-                resolve(1);
-            });
-        })
-        .task(function (next, prev) {
-            // 2 + 1
-            next(null, 2 + prev);
-        })
-        .taskSync(function (prev) {
-            // 3 + 3
-            return 3 + prev;
-        })
-        .serial()
-        .try(function (result) {
-            expect(result).toBe(6);
-            done();
-        });
-});
-
-it('.taskPromise + .task + .taskSync + .each', function (done) {
-    plan
-        .taskPromise(function () {
-            return new Promise(function (resolve) {
-                // 1
-                resolve(1);
-            });
-        })
-        .task(function (next, prev) {
-            // 2 + 1
-            next(null, 2 + prev);
-        })
-        .taskSync(function (prev) {
-            // 3 + 3
-            return 3 + prev;
-        })
-        .each([4, 5], function (index, val, next, prev) {
-            // 第 1 次：4 + 6
-            // 第 2 次：5 + 10
-            next(null, val + prev);
-        })
-        .serial()
-        .try(function (result) {
-            expect(result).toBe(15);
-            done();
-        });
-});
-
-it('.taskPromise + .task + .taskSync + .each + .eachSync', function (done) {
-    plan
-        .taskPromise(function () {
-            return new Promise(function (resolve) {
-                // 1
-                resolve(1);
-            });
-        })
-        .task(function (next, prev) {
-            // 2 + 1
-            next(null, 2 + prev);
-        })
-        .taskSync(function (prev) {
-            // 3 + 3
-            return 3 + prev;
-        })
-        .each([4, 5], function (index, val, next, prev) {
-            // 第 1 次：4 + 6
-            // 第 2 次：5 + 10
-            next(null, val + prev);
-        })
-        .eachSync([6, 7], function (index, val, prev) {
-            // 第 1 次：6 + 15
-            // 第 2 次：7 + 21
-            return val + prev;
-        })
-        .serial()
-        .try(function (result) {
-            expect(result).toBe(28);
-            done();
-        });
-});
-
-it('.taskPromise + .task + .taskSync + .each + .eachSync + .eachPromise', function (done) {
-    plan
-        .taskPromise(function () {
-            return new Promise(function (resolve) {
-                // 1
-                resolve(1);
-            });
-        })
-        .task(function (next, prev) {
-            // 2 + 1
-            next(null, 2 + prev);
-        })
-        .taskSync(function (prev) {
-            // 3 + 3
-            return 3 + prev;
-        })
-        .each([4, 5], function (index, val, next, prev) {
-            // 第 1 次：4 + 6
-            // 第 2 次：5 + 10
-            next(null, val + prev);
-        })
-        .eachSync([6, 7], function (index, val, prev) {
-            // 第 1 次：6 + 15
-            // 第 2 次：7 + 21
-            return val + prev;
-        })
-        .eachPromise([8, 9], function (index, val, prev) {
-            return new Promise(function (resolve) {
-                // 第 1 次：8 + 28
-                // 第 2 次：9 + 36
-                resolve(val + prev);
-            });
-        })
-        .serial()
-        .try(function (result) {
-            expect(result).toBe(45);
-            done();
-        });
-});
-
-
