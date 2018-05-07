7403c83a8bc9390883c412ea32a945349bca1ee3
#云淡然 <cloudcome@163.com>
2018-05-07 14:17:22 +0800

    ~ 单元测试

---
+++ test/rewrite.js
@@ -0,0 +1,37 @@
+/**
+ * 文件描述
+ * @author ydr.me
+ * @create 2018-04-27 15:58
+ * @update 2018-04-27 15:58
+ */
+
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+
+var dirname = path.join(__dirname, 'serial');
+var list = fs.readdirSync(dirname);
+
+list.forEach(function (name) {
+    var file1 = path.join(dirname, name);
+    // var content = fs.readFileSync(file1, 'utf8');
+    // var nameList = name.split(/[-.]/g);
+    var file2 = path.join(dirname, 'test.' + name);
+
+    fs.renameSync(file1, file2);
+    // nameList.shift();
+    // nameList.pop();
+    //
+    // content = content.replace(/^\s*?it\(/m, function (source) {
+    //     return '\n\ndescribe(\'' + nameList.join(' ') + '\', function () {\n' + source;
+    // }).replace(/\n\n$/, '') + '\n});\n\n';
+    //
+    // fs.writeFileSync(file1, content, 'utf8');
+});
+
+console.log(dirname, '重写成功');
+
+
+
+
--- test/test.index.js
+++ test/test.index.js
@@ -7,51 +7,6 @@
 
 'use strict';
 
-describe('串行任务', function () {
-    require('./serial/async');
-    require('./serial/sync');
-    require('./serial/all');
-});
+describe('index', function () {
 
-describe('并行任务', function () {
-    require('./parallel/async');
-    require('./parallel/sync');
-    require('./parallel/all');
-});
-
-describe('竞速任务', function () {
-    require('./race/async');
-    require('./race/sync');
-    require('./race/all');
-});
-
-describe('任意任务', function () {
-    require('./any/async');
-    require('./any/sync');
-    require('./any/all');
-});
-
-describe('事件', function () {
-    require('./events/serial');
-    require('./events/parallel');
-});
-
-describe('计划', function () {
-    require('./plan/is');
-    require('./plan/as');
-    require('./plan/parallel');
-    require('./plan/serial');
-});
-
-describe('promise', function () {
-    require('./promise/serial');
-    require('./promise/parallel');
-});
-
-describe('边界', function () {
-    require('./marginal/serial');
-    require('./marginal/parallel');
-    require('./marginal/promise');
-    require('./marginal/race');
-    require('./marginal/any');
 });
